771be8a50aea7d8c7fcfb52bde531820a07fe6c3
seosd <seosd97@gmail.com>
2020-05-11 18:50:39 +0900

    seeder 수정

--- seeders/20200506094719-initial-groups.js
+++ seeders/20200506094719-initial-groups.js
@@ -6,18 +6,21 @@ module.exports = {
             'TournamentGroups',
             [
                 {
+                    id: 1,
                     name: 'dck season 1',
                     sid: 1,
                     createdAt: new Date(),
                     updatedAt: new Date()
                 },
                 {
+                    id: 2,
                     name: 'dck season 2',
                     sid: 2,
                     createdAt: new Date(),
                     updatedAt: new Date()
                 },
                 {
+                    id: 3,
                     name: 'dck season 3',
                     sid: 3,
                     createdAt: new Date(),
