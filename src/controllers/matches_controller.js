00b440b0e48c5fcb2b3a733cc1b1da58115921ad
seosd <seosd97@gmail.com>
2020-07-30 18:40:17 +0900

    fix matches router

--- src/controllers/matches_controller.js
+++ src/controllers/matches_controller.js
@@ -1,29 +1,28 @@
-const { Match, Team, Summoner, TeamHistory, SummonerHistory, BanHistory } = require('../../models');
+const {
+    MatchGroup,
+    Match,
+    Team,
+    Summoner,
+    TeamHistory,
+    SummonerHistory,
+    BanHistory
+} = require('../../models');
 const match = require('../../models/match');
 
 exports.getAllMatches = async (req, res) => {
-    const matches = await Match.findAll({
+    const matches = await MatchGroup.findAll({
+        include: {
+            model: Match,
+            attributes: {
+                exclude: ['id', 'MatchGroupId', 'createdAt', 'updatedAt']
+            }
+        },
         attributes: {
-            exclude: ['createdAt', 'updatedAt']
+            exclude: ['id', 'createdAt', 'updatedAt']
         }
     });
 
     res.json(matches);
-};
-
-exports.getMatchById = async (req, res) => {
-    const matchId = parseInt(req.params.id, 10);
-    const matchData = await Match.findOne({
-        where: {
-            id: matchId
-        },
-        attributes: {
-            exclude: ['createdAt', 'updatedAt']
-        },
-        raw: true
-    });
-
-    res.json(matchData);
 };
 
 exports.getMatchByGameId = async (req, res) => {
@@ -42,10 +41,17 @@ exports.getMatchByGameId = async (req, res) => {
 };
 
 exports.getMatchesByType = async (req, res) => {
-    const matchDatas = await Match.findAll({
+    const tournamentId = parseInt(req.params.tournament_id, 10);
+    const matchDatas = await MatchGroup.findAll({
         where: {
-            TournamentId: req.params.tournament_id,
+            TournamentId: tournamentId,
             type: req.params.type
+        },
+        include: {
+            model: Match,
+            attributes: {
+                exclude: ['id', 'MatchGroupId', 'createdAt', 'updatedAt']
+            }
         },
         attributes: {
             exclude: ['createdAt', 'updatedAt']
@@ -60,23 +66,22 @@ exports.getMatchDetail = async (req, res) => {};
 
 exports.getMatchDetailByGameId = async (req, res) => {
     try {
+        const gameId = parseInt(req.params.game_id, 10);
         const matchData = await Match.findOne({
             where: {
-                game_id: req.params.game_id
+                game_id: gameId
             },
             attributes: {
-                exclude: ['createdAt', 'updatedAt']
+                exclude: ['MatchGroupId', 'createdAt', 'updatedAt']
             },
             include: [
                 {
                     model: TeamHistory,
-                    // as: 'teams',
                     attributes: {
                         exclude: ['createdAt', 'updatedAt']
                     },
                     include: {
                         model: BanHistory,
-                        // as: 'bans',
                         attributes: {
                             exclude: ['createdAt', 'updatedAt']
                         }
@@ -84,7 +89,6 @@ exports.getMatchDetailByGameId = async (req, res) => {
                 },
                 {
                     model: SummonerHistory,
-                    // as: 'summoner_stats',
                     attributes: {
                         exclude: ['createdAt', 'updatedAt']
                     }
@@ -99,50 +103,11 @@ exports.getMatchDetailByGameId = async (req, res) => {
     }
 };
 
-exports.getMatchBySummoner = async (req, res) => {
-    const summoner = await Summoner.findOne({
-        where: {
-            uuid: req.params.uuid
-        }
-    });
-
-    if (summoner === null) {
-        res.json({ status: 'error', msg: 'invalid uuid' });
-        return;
-    }
-
-    const histories = await summoner.getSummonerHistorys();
-
-    let payload = [];
-    for (let i in histories) {
-        const match = await histories[i].getMatch({
-            attributes: {
-                exclude: ['createdAt', 'updatedAt']
-            }
-        });
-
-        const data = await makeMatchData(match);
-        payload.push(data);
-    }
-
-    // if (matches === null) {
-    //     console.log('cant\'t find matches');
-    //     res.json({ status: 'error', msg: 'cant\'t find matches' });
-    //     return;
-    // }
-
-    // for (let i in matches) {
-    //     const match = await makeMatchData(matches[i]);
-    //     payload.push(match);
-    // }
-
-    res.json(payload);
-};
-
 exports.getMatchesByTournamentId = async (req, res) => {
+    const tournamentId = parseInt(req.params.tournament_id, 10);
     const matches = await Match.findAll({
         where: {
-            TournamentId: req.params.tournament_id
+            TournamentId: tournamentId
         },
         attributes: {
             exclude: ['createdAt', 'updatedAt']
@@ -153,79 +118,10 @@ exports.getMatchesByTournamentId = async (req, res) => {
     res.json(matches);
 };
 
-const makeMatchData = async matchData => {
-    let payload = {};
-    if (matchData === null) {
-        console.log('match data is null');
-        payload.status = {
-            code: 404,
-            msg: 'invlaid match id'
-        };
-        return payload;
-    }
-
-    payload = matchData.toJSON();
-
-    let teamDTOs = [];
-    const teamDatas = await matchData.getTeamHistories({
-        attributes: {
-            exclude: ['createdAt', 'updatedAt']
+exports.getSummonerMatchList = async (req, res) => {
+    const summoner = await Summoner.findOne({
+        where: {
+            name: req.params.name
         }
     });
-
-    for (let i in teamDatas) {
-        const teamData = teamDatas[i];
-        let dto = teamData.toJSON();
-        const team = await Team.findOne({
-            where: {
-                id: teamData.TeamId
-            }
-        });
-
-        const bans = await BanHistory.findAll({
-            where: {
-                TeamId: teamData.TeamId,
-                MatchId: matchData.id
-            },
-            attributes: {
-                exclude: ['MatchId', 'TeamId', 'createdAt', 'updatedAt']
-            },
-            raw: true
-        });
-
-        let teamKills = 0;
-        let teamDeaths = 0;
-        let teamAssists = 0;
-        let teamGold = 0;
-        let teamName = 'NULL';
-        if (team !== null) {
-            teamName = team.name;
-        }
-
-        dto.kills = teamKills;
-        dto.deaths = teamDeaths;
-        dto.assists = teamAssists;
-        dto.teamGold = teamGold;
-        dto.teamName = teamName;
-        dto.bans = bans;
-
-        teamDTOs.push(dto);
-    }
-
-    const summonerDatas = await matchData.getSummonerHistorys({
-        attributes: {
-            exclude: ['summoner_uuid', 'createdAt', 'updatedAt']
-        },
-        include: {
-            model: Summoner,
-            attributes: {
-                exclude: ['createdAt', 'updatedAt']
-            }
-        }
-    });
-
-    payload.teams = teamDTOs;
-    payload.summoners = summonerDatas;
-
-    return payload;
 };
--- src/routes/routers.js
+++ src/routes/routers.js
@@ -39,9 +39,9 @@ router.get('/tournaments/:id', ctrl_tournament.getTournamentData);
 
 // match
 router.get('/matches', ctrl_match.getAllMatches);
+router.get('/matches/by_tournament/:id', ctrl_match.getMatchesByTournamentId);
 router.get('/matches/:game_id', ctrl_match.getMatchByGameId);
 router.get('/matches/detail/:game_id', ctrl_match.getMatchDetailByGameId);
-router.get('/matches/by_tournament/:id', ctrl_match.getMatchesByTournamentId);
 router.get('/matches/matchlist/team/:team_name');
 router.get('/matches/matchlist/summoner/:summoner_name');
 
