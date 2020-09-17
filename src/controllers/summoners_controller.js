e37f65c980ccf6da4de2b5444b6351501f29e536
seosd <seosd97@gmail.com>
2020-09-17 21:40:12 +0900

    add summoner match history routes

--- src/controllers/summoners_controller.js
+++ src/controllers/summoners_controller.js
@@ -1,6 +1,6 @@
-const { Summoner, Team, SummonerHistory } = require('../../models');
-const summonerhistory = require('../../models/summonerhistory');
+const { Summoner, Team, SummonerHistory, MatchParticipant, Match } = require('../../models');
 const Sequelize = require('sequelize');
+const match = require('../../models/match');
 
 exports.getAllSummoners = async (req, res) => {
     const datas = await Summoner.findAll({
@@ -60,6 +60,8 @@ exports.getSummonerDataByName = async (req, res) => {
         return;
     }
 
+    data.statics = await this.getSummonerMostChampion(data.uuid);
+    data.matches = await this.getSummonerMatches(data.uuid);
     res.json(data);
 };
 
@@ -143,4 +145,59 @@ exports.getSummonerStat = async (req, res) => {
     });
 
     res.json(stats);
+};
+
+exports.getSummonerMostChampion = async uuid => {
+    const stat = await SummonerHistory.findAll({
+        where: { summoner_uuid: uuid },
+        attributes: [
+            'cid',
+            [Sequelize.fn('count', Sequelize.col('id')), 'games'],
+            [Sequelize.fn('sum', Sequelize.col('kill')), 'kills'],
+            [Sequelize.fn('sum', Sequelize.col('death')), 'deaths'],
+            [Sequelize.fn('sum', Sequelize.col('assist')), 'assists'],
+            [Sequelize.fn('sum', Sequelize.col('win')), 'wins'],
+            [Sequelize.fn('sum', Sequelize.literal('case when win then 0 else 1 end')), 'defeats'],
+            [Sequelize.fn('avg', Sequelize.col('visionScore')), 'visionScore'],
+            [
+                Sequelize.fn(
+                    'avg',
+                    Sequelize.literal(
+                        '(coalesce(totalMinionsKilled, 0) + coalesce(neutralMinionsKilled, 0))'
+                    )
+                ),
+                'cs'
+            ]
+        ],
+        group: 'cid',
+        raw: true
+    });
+
+    return stat;
+};
+
+exports.getSummonerMatches = async uuid => {
+    const stats = await SummonerHistory.findAll({
+        where: { summoner_uuid: uuid },
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
+        raw: true
+    });
+
+    const ids = stats.map(m => m.match_id);
+    const matches = await Match.findAll({
+        where: { game_id: ids },
+        include: {
+            model: MatchParticipant,
+            as: 'participants',
+            attributes: { exclude: ['createdAt', 'updatedAt'] }
+        }
+    });
+
+    const result = matches.map(m => {
+        let match = m.toJSON();
+        match.stat = stats.find(s => s.match_id === m.game_id);
+        return match;
+    });
+
+    return result;
 };
