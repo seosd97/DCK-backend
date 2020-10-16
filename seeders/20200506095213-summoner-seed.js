8a280f5f2492da720deca4b1ee2948f6edf8be4a
seosd <seosd97@gmail.com>
2020-10-16 22:52:32 +0900

    fix summoner seed name to uuid

--- seeders/20200506095213-summoner-seed.js
+++ seeders/20200506095213-summoner-seed.js
@@ -2,37 +2,37 @@
 
 const game_api = require('../src/lol-api/game-api');
 const userList = [
-    '제퇴기',
-    '수박송송',
-    '정확신속고급남탓',
-    '석진인데요',
-    '하리흰',
-    '주님하나더가요',
-    '잔잔맨',
-    'Unslaad Brii',
-    '12시 20분',
-    '편안한 승차감',
-    'Dopa kr',
-    '오재용',
-    'BlTCH Vayne',
-    'NaDiapla',
-    '라면꼬들꼬들',
-    '183cm 미남',
-    'ZEDDDD99',
-    '김체원',
-    'GlenCheck',
-    '안전한 이불안',
-    '하얀냥',
-    '팀운좋아질꺼얌',
-    '세계최강양희성',
-    '로리아리',
-    '예니하',
-    '룰루는 버스나 타',
-    '매우어려운게임',
-    '베이스볼',
-    'Reyes',
-    '1031 Song',
-    '태0꿍'
+    '_LSLqZRd4kCyuNMtPtiXPj7GBewd1z9EsqJeE8tMJMn99w',
+    '07AY4_jm3qDZnWzDZqTPzU9ed3TRapqniZeEQ5xu8whgKZQ',
+    '1INgWfZyrwXq8U7q9GJ5CYe4VtIzd4pTuvJ0ZYpaICI_Qg',
+    '3cSMzHxcw_fVyagmoPdQJioiHJLTD7QdwJN4mzAduY8KC70',
+    '45sm6ZKvSw4lU20rGsaNBDZoDBv0okXYXG3Jemtd25AoS6E',
+    '8nliV-V783RRFNX44aU8PJaxIIx2NOhcU3m-oJzU8vETFoA',
+    'As1OE9KJJ10fcQQL2gM45JBbvctAhFFKXt9-RTeql582BA',
+    'cJ-AUvUbrYSRUR7l0Q0a555gAsM7N9lMVecnTrcZt0n6_mE',
+    'CO-rGAwswdYIlYCHRoozdn6_DUr-JYxnc0-DiN-Y_sWsyw',
+    'D_tvCWAr5_LuNGuWupB1C8IbKhstpFk4H2hsEY4mvrtLNQ',
+    'k42NvYLWEHRVQ7Abjv6aUWZmHYjYeOlgB9Zem28pxrfhOg',
+    'LLhLI2X_vQYpcItLgwo0tGMm2MU_IkYc7bPc3CaNM0E7Qx8',
+    'MBno6ryqTt0Xij1QInqpK-jdzAxO2jcrFItjJywNhQKA-A',
+    'mJAaG546gLR9hl5sYtWTuAkVICWJ9NZrL1yERqBRwvsCf-o',
+    'ny-C80Q6N3sdsV2TWuajx3vc9vA46v_KmQbFxKMOv-r2UA',
+    'oh5ClPyijzV2g8ikeE0o1Bb_TbdeIeNzvy2V8IZy5VERCA',
+    'Plnw8V_OJ5crZH-JxyWBpenfwFjMq2yeYepeOoKiDnjQ2b0',
+    'PWxoR5Yw7yQ44Lgeq4_-Z58C6Lr4km65HO-miYhs2iUy_w',
+    'RIBnRwOof_qHcYbOurqxIEeyzu0Iw7_rD1h6S_FUnlxuXA',
+    'SbUkq2j3PQRhdqlI7PxEDLpOGOywN3vXvrZgt2pMlrvwFA',
+    'SG60blS_xjmWSmZbU6dQEPBRZSupscfwF7UunodHppQNjHU',
+    'slmHI1IPQ9pAxh-8PtqoRzgcg48S7qRgFDNQxu2Xb23wtGQ',
+    'TbDkzPibrQFFeq1N3TbPQA-VEG9zvgG8WQ0tWPGdpkIGBaA',
+    'u3-xDuezI_CBWP6h6B_vbBRb6BuuMy52eRWqQjdU-DcoJSg',
+    'vyZLe7D--lpwDJrqhW97jUc7HuDUvR5ATVjuKgyyiHCQFl8',
+    'XlPdgGpiqLKIW6l2rpQ22iZyYGDwkbcdWzNMEci0yMNz1Q',
+    'yBe7u6byd3nZqGfqxmRY-0VC06_lNVehcqmD-kuDATjaZW8',
+    'yGAQ1GYM56tONXv5w0jQFV7USHTgbX2h8Bw8Qh0Zegaj1bo',
+    'Yp7txav8JxBv1OF_dkR2qe_BGoKm-tz4ITGMWxb_PNCI3Q',
+    'ZXeSmxd08fxCJktqse5gsPbJ0zpdg9RLy2hIit8nm0I9Qg',
+    'zY4vVZ981vqRqm0RaN9XOCysTIFD5wCiaB3PhZOvjRdaAQ'
 ];
 
 module.exports = {
@@ -40,7 +40,7 @@ module.exports = {
         let result = [];
 
         for (let i in userList) {
-            const res = await game_api.getSummonerByName(userList[i]);
+            const res = await game_api.getSummoner(userList[i]);
 
             const data = JSON.parse(res);
             const userData = {
