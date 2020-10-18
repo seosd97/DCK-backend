0c38fcc4983deb0c0bb34e7bf8b1c3432cb0ebad
seosd <seosd97@gmail.com>
2020-10-18 16:08:39 +0900

    set https option

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const https = require('https');
+const http = require('http');
 const cors = require('cors');
+const fs = require('fs');
 const dataApi = require('./lol-api/data-api');
 const app = express();
 const router = require('./routes/routers');
 const db = require('./../models').sequelize;
-const port = 8080;
+const port = process.env.SERVER_PORT || 443;
 
 app.use(cors());
 app.use(router);
@@ -31,6 +34,18 @@ const init = async () => {
     await init();
 })();
 
-app.listen(port, () => {
-    console.log(`express listening at port ${port}`);
-});
+const option =
+    process.env.NODE_ENV === 'production'
+        ? {
+            key: fs.readFileSync('/opt/bitnami/apache2/conf/api.dcklol.com.key'),
+            cert: fs.readFileSync('/opt/bitnami/apache2/conf/api.dcklol.com.crt')
+        }
+        : undefined;
+
+option
+    ? https.createServer(option, app).listen(port, () => {
+        console.log(`http server on port ${port}`);
+    })
+    : http.createServer(app).listen(port, () => {
+        console.log(`http server on port ${port}`);
+    });
